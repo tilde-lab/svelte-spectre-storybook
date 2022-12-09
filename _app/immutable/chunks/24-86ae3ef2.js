23785b7fe643098ad0f57e7b91c6ca129022308e
blokhin <blokhin@users.noreply.github.com>
2022-12-09 09:59:30 +0000

    Deploying to gh-pages from @ tilde-lab/svelte-spectre-storybook@a4acc57f3a9ad4c12a6cc455a05f76aab971a139 🚀

---
+++ _app/immutable/chunks/24-86ae3ef2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/docs/components/periodictable/_page.md-3f2dae0e.js";export{t as component};
--- _app/immutable/chunks/24-f424850e.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/docs/components/periodictable/_page.md-69cc22e0.js";export{t as component};
